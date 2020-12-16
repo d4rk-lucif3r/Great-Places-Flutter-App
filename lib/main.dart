77a8ebcdcdcdbc73de0551392c46e64f107c3a82
Arsh Anwar <lucifer78908@gmail.com>
2020-12-16 14:03:23 +0530

    renamed typo
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.red,
         ),
-        home: PlacesList()
+        home: PlacesListScreen()
       ),
     );
   }
