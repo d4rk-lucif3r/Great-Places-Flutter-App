02cfec2e182b5a77cf4bd60f9887eee2d5984375
Arsh Anwar <lucifer78908@gmail.com>
2020-12-16 14:02:05 +0530

    added screens,providers,models
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: PlacesList()
       ),
-      //home: ...
     );
   }
 }
-
- 
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
