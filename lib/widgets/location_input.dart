032e52b9be1e457020f770816a8a08711153ffcc
Arsh Anwar <lucifer78908@gmail.com>
2020-12-16 18:16:51 +0530

    Implemented collecting current latitude and longitude
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -7,6 +8,11 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
+  Future<void> _getCurrentLocation() async {
+    final locaData = await Location().getLocation();
+    print(locaData.latitude);
+    print(locaData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +44,7 @@ class _LocationInputState extends State<LocationInput> {
           FlatButton.icon(
             icon: Icon(Icons.location_on),
             label: Text('Current Location'),
-            onPressed: () {},
+            onPressed: _getCurrentLocation,
             textColor: Theme.of(context).primaryColor,
           ),
           FlatButton.icon(
